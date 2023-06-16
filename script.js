4a7840047e7830edd0a082a382b4884bec88b35f
kunxin-chor <chorkunxin@yahoo.com>
2023-06-16 17:58:25 +0000

    Added updating cache code

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
+
+let globalRegistration = null;
 if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function() {
-        navigator.serviceWorker.register('service-worker.js').then(function(registration) {
+    window.addEventListener('load', function () {
+        navigator.serviceWorker.register('service-worker.js').then(function (registration) {
             console.log('ServiceWorker registration successful with scope: ', registration.scope);
-        }, function(err) {
+            globalRegistration = registration;
+        }, function (err) {
             console.log('ServiceWorker registration failed: ', err);
         });
+    });
+
+    navigator.serviceWorker.addEventListener('controllerchange', () => {
+        console.log("new service worker registered");
+        // window.location.reload();    
     });
 }
 
 function updateCache() {
-    if (navigator.serviceWorker && registration.active) {
-        registration.active.postMessage({ type: 'UPDATE_CACHE' });
+    if (navigator.serviceWorker && globalRegistration.active) {
+     
+        globalRegistration.active.postMessage({ type: 'UPDATE_CACHE' });
+       
     }
 }
--- service-worker.js
+++ service-worker.js
@@ -37,7 +37,9 @@ self.addEventListener('fetch', (event) => {
 });
 
 self.addEventListener('message', (event) => {
+   
     if (event.data && event.data.type === 'UPDATE_CACHE') {
-        self.skipWaiting();
+   
+        self.skipWaiting();             
     }
 });
